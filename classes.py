8df9c37bff92dbef21f0557833c6397a37c0b4f0
Alex Solomaha <cyanofresh@gmail.com>
2020-02-20 21:31:56 +0200

    classes

---
+++ classes.py
@@ -0,0 +1,15 @@
+class Task:
+    def __init__(self, books_num, libs_num, days):
+        self.books_num = books_num
+        self.libs_num = libs_num
+        self.days = days
+
+
+class Library:
+    def __init__(self, ID, books_num, signup_time, books_per_day, books):
+        self.ID = ID
+        self.books_num = books_num
+        self.signup_time = signup_time
+        self.books_per_day = books_per_day
+        self.books = books
+
