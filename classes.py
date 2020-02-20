68d79ef36c0cb82125f56036148e1c71235ff5d9
Olexandr Opanasevych <39926420+AlexOpanasevych@users.noreply.github.com>
2020-02-20 22:55:55 +0200

    Add files via upload

--- classes.py
+++ classes.py
@@ -5,11 +5,20 @@ class Task:
         self.days = days
 
 
+class Book:
+    def __init__(self, ID, score):
+        self.ID = ID
+        self.score = score
+
+
 class Library:
-    def __init__(self, ID, books_num, signup_time, books_per_day, books):
+    def __init__(self, ID : int, books_num : int, signup_time : int, books_per_day : int, books):
         self.ID = ID
         self.books_num = books_num
         self.signup_time = signup_time
         self.books_per_day = books_per_day
         self.books = books
+
+    def calc_books_score(self):
+        return sum(book.score for book in self.books)
 
--- scorecounter.py
+++ scorecounter.py
@@ -1,28 +1,22 @@
-import sys
+import reader
 
 class ScoreCounter:
-    def __init__(self, filename):
-        self.filename = filename
-        with open(filename, mode='r') as inp:
-            self.count_of_lib = int(inp.readline())
-            self.inp = inp
+    def __init__(self, libraries):
+        self.libraries = libraries
         self.score = 0
 
 
     def count(self):
         repeated = []
-        with open(self.filename, mode='r') as inp:
-            inp.readline()
-            for i in range(self.count_of_lib):
-                list_from_f_l = [int(token) for token in inp.readline().split(' ')]
-                list_from_s_l = [int(token) for token in inp.readline().split(' ')]
-                for j in range(int(list_from_f_l[1])):
-                    if int(list_from_s_l[j]) not in repeated:
-                        repeated.append(int(list_from_s_l[j]))
-                        self.score += 1
+        for library in self.libraries:
+            for book in library.books:
+                if book.ID not in repeated:
+                    repeated.append(book.ID)
+                    self.score += book.score
         return self.score
 
 
 if __name__ == "__main__":
-    sc = ScoreCounter('input_set.txt')
+    file_content = reader.parse_from('a_example.txt')
+    sc = ScoreCounter(file_content)
     print(sc.count())
