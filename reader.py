76705f1a1f9c6062596deb9603d17a5c78069918
JeyM1 <denafen2000@gmail.com>
2020-02-20 21:53:44 +0200

    +parse_from()

--- reader.py
+++ reader.py
@@ -1,9 +1,19 @@
+import classes
+
+
 def parse_from(filename):
     with open(filename) as fin:
-        books_count, libs_count, max_days = (int(i) for i in fin.readline().split(" "))
-    books_scores = (int(i) for i in fin.readline().split(" "))
-
+        input = fin.readlines()
+    books_count, libs_count, max_days = (int(i) for i in input[0].split(" "))
+    books_scores = (int(i) for i in input[1].split(" \n"))
+    libs = []
+    for i in range(libs_count):
+        lib_books_count, signup_time, books_ship_count = (int(i) for i in input[2 + i * 2].split(" "))
+        lib_books = [int(i) for i in input[3 + i * 2].split(" ")]
+        libs.append(classes.Library(i, lib_books_count, signup_time, books_ship_count, lib_books))
+    return libs
 
 
 if __name__ == "__main__":
-    parse_from("a_example.txt")
+    libs = parse_from("a_example.txt")
+    print(libs[0].books)
